b3e9ffafd796351a969e259af862b2cd4eae43b6
MHaasjes <m.haasjes@gmail.com>
2024-05-15 22:12:11 +0200

    Update and rename pve-installer-rpi-14-05-2024.sh to pve-installer-rpi-15-05-2024.sh

--- pve-installer-rpi-15-05-2024.sh
+++ pve-installer-rpi-15-05-2024.sh
@@ -6,32 +6,6 @@ if [ $(whoami) != "root" ]; then
   exit 1
 fi
 
-# Ask the user for the new server ip address
-echo "Enter the ip address for the new server:"
-read newipserver
-
-# Validate the user's input
-if [ -z "$newipserver" ]; then
-  echo "You did not enter a server ip address. The script is stopping."
-  exit 1
-fi
-
-# Ask the user for the new gateway ip address
-echo "Enter the ip address for the new gateway:"
-read newipgateway
-
-# Validate the user's input
-if [ -z "$newipgateway" ]; then
-  echo "You did not enter a server ip address. The script is stopping."
-  exit 1
-fi
-
-sudo nmcli c mod "Wired connection 1" ipv4.addresses "$newipserver"/24 ipv4.method manual
-sudo nmcli con mod "Wired connection 1" ipv4.gateway "$newipgateway"
-sudo nmcli con mod "Wired connection 1" ipv4.dns 8.8.8.8
-sudo nmcli c down "Wired connection 1" && sudo nmcli c up "Wired connection 1"
-
-
 # Ask the user for the new server name
 echo "Enter the new server name:"
 read newservername
