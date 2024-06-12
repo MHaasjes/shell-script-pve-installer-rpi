1a22227046cdbb2750e2e2b48aa7d056c7ea9ee3
MHaasjes <m.haasjes@gmail.com>
2024-06-12 11:09:54 +0200

    Update pve-installer-rpi-2.1.sh

--- pve-installer-rpi-2.1.sh
+++ pve-installer-rpi-2.1.sh
@@ -114,10 +114,10 @@ sudo sed -i "s/100/2048/g" /etc/dphys-swapfile
 sudo dphys-swapfile setup
 sudo dphys-swapfile swapon
 
-sudo apt install ifupdown2 -y
-
 
 #/etc/network/interfaces
+sudo apt install ifupdown2 -y
+
 sudo sed -i -e '$ a\auto lo\niface lo inet loopback \n \niface eth0 inet manual \n \nauto vmbr0 \niface vmbr0 inet static \n        address newipserver/24 \n        gateway newipgateway \n        bridge-ports eth0 \n        bridge-stp off \n        bridge-fd 0' /etc/network/interfaces
 sudo sed -i "s/newipserver/$newipserver/g" /etc/network/interfaces
 sudo sed -i "s/newipgateway/$newipgateway/g" /etc/network/interfaces
